67dbe554ffb1e844b0442b933b50ba43b37e0f23
Isaie Courilleau <icourilleau@hotmail.fr>
2016-07-26 16:37:40 +0000

    make it better

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 var express = require("express");
+var moment = require("moment");
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/:url', function(req, res){
+  try{
+    decodeURIComponent(req.url.substring(0))
+  } catch(e){
+    console.log("error");
+  }
+  var reqUrl = req.url.substring(1);
+  if(!isNaN(parseFloat(reqUrl))){
+    var natural = moment.unix(reqUrl).format("MMM DD,YYYY");
+    var unix = reqUrl;
+  } else if (moment(decodeURI(reqUrl), "MMM DD,YYYY").isValid()) {
+    natural = decodeURI(reqUrl);
+    unix = moment(natural,"MMM DD,YYYY").unix();
+  } else {
+    natural = null;
+    unix = null;
+  }
+  var answer = JSON.stringify({unix: unix,natural: natural});
+  res.send(answer);
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT, function () {
+  console.log('Server listening on $PORT!');
 });
